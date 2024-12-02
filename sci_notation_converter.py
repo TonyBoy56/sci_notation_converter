35a52388b812a1b0f39d394d0a8fbfd2229626d8
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-12-02 11:55:40 -0800

    create a function that will accept a user's input.

--- sci_notation_converter.py
+++ sci_notation_converter.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# Accept input from the user
+def accept_input():
+    name = input("Hello, user. Please enter your name: ")
+    
+    if name:
+        print(f"Excellent. Nice to meet you {name}. Let's get started.")
+    
+    
+    
+# Convert user's input to scientific notation
+# Convert input of scientific notation value to floating-point number
+
+accept_input()
