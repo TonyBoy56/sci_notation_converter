1102baba0016cf0a521363423d2823fc356d27da
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-12-03 12:27:39 -0800

    implement a try/except block to first attempt the conversion of a string to a float.

--- sci_notation_converter.py
+++ sci_notation_converter.py
@@ -12,9 +12,13 @@ def accept_input():
     name = input("Hello, user. Please enter your name: ")
     if name:
         print(f"Excellent. Nice to meet you {name}. Let's get started.")
-        user_input = float(input("Enter a number in scientific notation. I will convert into a floating-point number for you: "))
+        user_input = input("Enter a number in scientific notation. I will convert into a floating-point number for you: ")
         
-        print(type(user_input))
+        try:
+            user_input = float(user_input)
+            print(type(user_input))
+        except Exception as e:
+            print(f"Invalid input occurred: {e}")
     else:
         print("Well then, have an nice day.")
     
