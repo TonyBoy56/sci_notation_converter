40bf23dc47b1470d20b2fd053a7535704d863b6a
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-12-03 12:39:08 -0800

    deligate keypress and string to float conversions to their own functions. Change the name of accept_input() function to init_user_input()

--- sci_notation_converter.py
+++ sci_notation_converter.py
@@ -1,29 +1,37 @@
 from pynput.keyboard import Key, Controller
 
 # Accept input from the user
-def accept_input():
-    keyboard = Controller()
-    
-    keyboard.press(Key.ctrl)
-    keyboard.press('`')
-    keyboard.release(Key.ctrl)
-    keyboard.release('`')
+
+
+def init_user_input():
+    user_keypress()    
     
     name = input("Hello, user. Please enter your name: ")
     if name:
         print(f"Excellent. Nice to meet you {name}. Let's get started.")
         user_input = input("Enter a number in scientific notation. I will convert into a floating-point number for you: ")
-        
-        try:
-            user_input = float(user_input)
-            print(type(user_input))
-        except Exception as e:
-            print(f"Invalid input occurred: {e}")
+        str_to_float(user_input)
     else:
-        print("Well then, have an nice day.")
+        print("Well then, have an nice day. Run the program to try again.")
+
+# Simulate Keyboard inputs
+def user_keypress():
+    keyboard = Controller()
+    keyboard.press(Key.ctrl)
+    keyboard.press('`')
+    keyboard.release(Key.ctrl)
+    keyboard.release('`')
+
+# Convert string input to float
+def str_to_float(input):
+    try:
+        input = float(input)
+        print(type(input), input)
+    except Exception as e:
+        print(f"Invalid input occurred: {e}")
     
     
 # Convert user's input to scientific notation
 # Convert input of scientific notation value to floating-point number
 
-accept_input()
+init_user_input()
